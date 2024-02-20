83513a76ad600f5c4e0a2ec790b0dc82bf56b0ab
HAL <hal@pop-os.localdomain>
2024-02-20 18:32:12 -0300

    finished unpack and folder name return

--- README.md
+++ README.md
@@ -8,3 +8,13 @@
     7- Create desktop entry file
     8- update desktop database
     4- Allows to change main dir path through arguments
+
+    ## V1
+
+    - Create jetbrains main dir at op/
+    - Unzip IDE into jetbrains main dir
+    - Create the desktop entry file 
+
+    ## V2
+
+    - Create symlink to enable the IDE to be called through CLI
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use core::panic;
 use std::fs::{self, File};
-use std::io::ErrorKind;
-use std::path::{Path, PathBuf};
-use std::time::Instant;
+use std::io::{self, ErrorKind};
+use std::path::{self, Path, PathBuf, StripPrefixError};
+use std::time::{Duration, Instant};
 
 use std::os::unix::fs::symlink;
+use std::thread::sleep;
 
 use clap::Parser;
 use flate2::read::GzDecoder;
+use spinoff::{Color, Spinner, spinners};
 use tar::Archive;
 
 #[derive(Parser)]
@@ -14,31 +17,52 @@ struct Cli {
     zip_path: std::path::PathBuf
 }
 
-fn unpack_tar(file_path: &PathBuf, dest_path: &Path) -> std::io::Result<()>{
-
-    println!("Start unpacking...");
+fn unpack_tar(file_path: &PathBuf, dest_path: &Path) -> Result<String, io::Error>{
 
     let start: Instant = Instant::now();
+    let mut spinner = Spinner::new(spinners::Binary, "Extracting files...", Color::Green);
 
     let file = File::open(file_path)?;
+    let file_read = File::open(file_path)?;
 
-    let tar = GzDecoder::new(file);
+    let mut archive = Archive::new(GzDecoder::new(file));
+    let mut archive_read = Archive::new(GzDecoder::new(file_read));
 
-    let mut archive = Archive::new(tar);
+    let mut ide_dir_name = String::new();
 
-    println!("This may take a while...");
-
-    archive.unpack(dest_path)?; // i need to pass the new address of the archive default_path + archiveName
-
-    let finish = Instant::now();
-
-    println!("Successful unpacked, took {:?}", finish.duration_since(start));
-
-    Ok(())
+    archive.unpack(dest_path)?;
+    for entry in archive_read.entries()? {
+        let mut entry = entry?;
+        if let Some(name) = entry.path()?.to_str() {
+            ide_dir_name = name.to_string();
+            break; // Exit loop after getting the first entry
+        }
+    };
+    spinner.stop_with_message(&*format!("✅ IDE successfully unpacked, took {:?}", start.elapsed()));
+    Ok(ide_dir_name)
 }
 
-
+// fn create_desktop_entry(){
+//     println!("test")
+//
+//     /*
+//         [Desktop Entry]
+//         Version=1.0
+//         Type=Application
+//         Name=IntelliJ IDEA
+//         Icon=/opt/idea-<version>/bin/idea.png
+//         Exec="/opt/idea-<version>/bin/idea.sh" %f
+//         Comment=Develop with pleasure!, The complete IDE crafted for Gophers, The Python IDE for Professional Developers, Focus on what matters
+//         Categories=Development;IDE;
+//         Terminal=false
+//         StartupWMClass=jetbrains-idea
+//      */
+//
+// }
+//
 fn main() -> std::io::Result<()>{
+    
+    // identify de IDE by path name ? by archive ? 
 
     let args = Cli::parse();
 
@@ -51,13 +75,15 @@ fn main() -> std::io::Result<()>{
         Ok(result) => result,
         Err(error) => match error.kind() {
             ErrorKind::AlreadyExists => println!("Skipping directory creation..."),
-            _ => { 
+            _ => {
                 panic!("Try running with sudo: {}", error)
             }
         }
     };
 
-    let _ = unpack_tar(&args.zip_path, &default_path);
+    let archive_name = unpack_tar(&args.zip_path, &default_path)?;
+
+    println!("archive: {}/{}", default_path.display(), archive_name.replace("/", ""));
 
     // Trying to create a symbolic link
     // symlink(original, link)
